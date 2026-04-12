901b322d102bdf1c8cdab8e1ab9eeedfe89cffb6
Anh Vu <anhvu01212001@gmail.com>
2026-04-12 03:03:06 -0500

    add settings and map

--- train_rl.py
+++ train_rl.py
@@ -23,7 +23,7 @@ def build_parser():
 
     parser.add_argument(
         "--model-output",
-        default="./configurations/rl_model_4corners.h5",
+        default="./configurations/rl_model_map.h5",
         help="Path to save the trained model.",
     )
     parser.add_argument(
