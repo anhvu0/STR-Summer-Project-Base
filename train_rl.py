9194c210263a4463d035f805a648930b6d4e1bf6
Anh Vu <anhvu01212001@gmail.com>
2026-04-12 01:59:38 -0500

    add new map

--- train_rl.py
+++ train_rl.py
@@ -23,13 +23,13 @@ def build_parser():
 
     parser.add_argument(
         "--model-output",
-        default="./configurations/rl_model_4corners.h5",
+        default="./configurations/rl_model_map.h5",
         help="Path to save the trained model.",
     )
     parser.add_argument(
         "--episodes",
         type=int,
-        default=330,
+        default=600,
         help="Number of training episodes.",
     )
     parser.add_argument(
