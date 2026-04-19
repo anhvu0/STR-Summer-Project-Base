7951b62a0290d002e8745cbd69bada680a694454
Anh Vu <anhvu01212001@gmail.com>
2026-04-19 08:24:43 -0500

    after phase 4

--- train_rl.py
+++ train_rl.py
@@ -29,7 +29,7 @@ def build_parser():
     parser.add_argument(
         "--episodes",
         type=int,
-        default=100,
+        default=300,
         help="Number of training episodes.",
     )
     parser.add_argument(
